68d4a2bc0f6e6d0c33964ffbe9058801d05621fd
Isaac <izaak343@gmail.com>
2018-11-12 08:18:42 -0600

    serial communication with servo

---
+++ servo-serial/servo-serial.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+int angle;
+int entry;
+Servo servo_one;
+
+void setup() {
+ Serial.begin(9600);
+ servo_one.attach(12); //pin must be PWM
+}
+
+void loop() {
+  if (Serial.available() >0){
+    entry = Serial.parseInt();
+    if(entry > 180){
+      servo_one.write(180);
+      Serial.println(entry);
+      }
+    else{
+      //angle = (entry*255)/180;
+      servo_one.write(entry);
+      Serial.println(entry);
+      }
+    
+    delay(10);
+    }
+  
+  
+}
