e4c7cf71d85b61271654a3102e6a12c700b4c99a
Isaac <izaak343@gmail.com>
2018-11-13 23:10:59 -0600

    changes in keypad

--- keypad/keypad.ino
+++ keypad/keypad.ino
@@ -4,7 +4,7 @@ const byte cols = 4;
 const byte pin_rows[]={10, 9, 8, 7};
 const byte pin_cols[]={6, 5, 4, 3};
 char keys[rows][cols]=
-  {{'1', '2', '3', 'A'},
+  {{1, '2', '3', 'A'},
    {'4', '5', '6', 'B'},
    {'7', '8', '9', 'C'},
    {'*', '0', '#', 'D'}};
